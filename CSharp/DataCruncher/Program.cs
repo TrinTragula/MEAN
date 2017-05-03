0864e1d6bdfba7e00581cf6135932b1a3afa70c7
TrinTragula <dcat@protonmail.com>
2017-05-04 00:37:53 +0200

    Lavori sulla gui

--- CSharp/DataCruncher/Program.cs
+++ CSharp/DataCruncher/Program.cs
@@ -18,14 +18,14 @@ namespace DataCruncher
             Thread.CurrentThread.CurrentCulture = ci;
             Thread.CurrentThread.CurrentUICulture = ci;
             // Argomenti di input
-            var file1 = args.Length == 0 ? "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch000.txt" : args[0];
-            var file2 = args.Length == 0 ? "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch001.txt" : args[1];
-            var xMin = args.Length == 0 ? 0 : Int32.Parse(args[2]);
-            var xMax = args.Length == 0 ? int.MaxValue : Int32.Parse(args[3]);
-            var yMin = args.Length == 0 ? 0 : Int32.Parse(args[4]);
-            var yMax = args.Length == 0 ? int.MaxValue : Int32.Parse(args[5]);
-            var nCanali = args.Length == 0 ? 1000 : Int32.Parse(args[6]);
-            var overwrite = args.Length == 0 ? false : Boolean.Parse(args[7]); ;
+            var nCanali = args.Length < 1 ? 1000 : Int32.Parse(args[0]);
+            var file1 = args.Length < 2 ? "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch000.txt" : args[1];
+            var file2 = args.Length < 3 ? "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch001.txt" : args[2];
+            var xMin = args.Length < 4 ? 0 : Int32.Parse(args[3]);
+            var xMax = args.Length < 5 ? int.MaxValue : Int32.Parse(args[4]);
+            var yMin = args.Length < 6 ? 0 : Int32.Parse(args[5]);
+            var yMax = args.Length < 7 ? int.MaxValue : Int32.Parse(args[6]);
+            var overwrite = args.Length < 8 ? false : Boolean.Parse(args[7]); ;
 
             //Creo o riutilizzo il database
             Database db = new Database("coincidenze_vere", overwrite);
--- Node/index.js
+++ Node/index.js
@@ -6,88 +6,113 @@ const child = require('child_process').execFile;
 const d3 = Plotly.d3;
 const formatter = d3.format('.2f');
 
-var path1 = "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch000.txt"
-var path2 = "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch001.txt"
-var executablePath = "DataCruncher/DataCruncher.exe"
-const nCanali = 1000;
+//let path1 = "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch000.txt"
+//let path2 = "C:\\Users\\Daniele\\Desktop\\Tesi Magistrale\\Dati\\Matrice152Eu_ch001.txt"
+let nCanali = $("#nCanali").val() || 1000;
 
-// logica di primo caricamento
-let params = [];
-child(executablePath, params, function (err, data) {
-  dataFile = fs.readFileSync("result.txt", 'ascii');
-  if (err) console.log("ERRORE: " + err)
-  console.log("Loaded");
-  dataLines = dataFile.split("\n");
-  metaData = dataLines.shift().split(" ");
-  interval = metaData[0];
-  min = metaData[1];
-  xData = dataLines.map(x => x.split(" ")[0] * 1);
-  yData = dataLines.map(x => x.split(" ")[1] * 1);
-  zData = dataLines.map(x => x.split(" ")[2] * 1);
-  dataVis.data[0].x = xData;
-  dataVis.data[0].y = yData;
-  dataVis.data[0].z = zData;
-  Plotly.redraw(dataVis);
+const executablePath = "DataCruncher/DataCruncher.exe"
+const dataVis = document.getElementById('dataVis');
+let firstTime = true;
+let x1, x2, y1, y2, interval, min;
+
+$("#drawButton").on("click", function (e) {
+  // frist load and reset logic
+  nCanali = $("#nCanali").val() || 1000;
+  path1 = document.getElementById("file1").files[0].path;
+  path2 = document.getElementById("file2").files[0].path;
+  console.log(path1, path2);
+  let params = [nCanali];
+  child(executablePath, params, function (err, fileData) {
+    dataFile = fs.readFileSync("result.txt", 'ascii');
+    if (err) console.log("ERRORE: " + err)
+    console.log("Loaded");
+    dataLines = dataFile.split("\n");
+    metaData = dataLines.shift().split(" ");
+    interval = metaData[0];
+    min = metaData[1];
+    xData = dataLines.map(x => x.split(" ")[0] * 1);
+    yData = dataLines.map(x => x.split(" ")[1] * 1);
+    zData = dataLines.map(x => x.split(" ")[2] * 1);
+    // I'll draw the matrix
+    let data = [{
+      x: xData,
+      y: yData,
+      z: zData,
+
+      /*type: 'histogram2dcontour',
+      line: {
+      	width: 0
+      },
+      contours: {
+      	coloring: 'heatmap'
+      },*/
+      type: 'heatmap',
+      colorscale: [
+        [0, 'rgb(200,200,255)'],
+        [0.01, 'rgb(0, 0, 255)'],
+        [0.25, 'rgb(0, 255, 0)'],
+        [0.5, 'rgb(200,55,0)'],
+        [1, 'rgb(255,0,0)']
+      ]
+    }];
+
+    let xAxisTemplate = {
+      range: [0, nCanali],
+      showgrid: true,
+      zeroline: true,
+      linecolor: 'black',
+      showticklabels: true,
+      ticks: ''
+    };
+    let yAxisTemplate = {
+      showgrid: true,
+      zeroline: true,
+      linecolor: 'black',
+      showticklabels: true,
+      ticks: ''
+    };
+    let layout = {
+      xaxis: xAxisTemplate,
+      yaxis: yAxisTemplate,
+      dragmode: 'select'
+    };
+    Plotly.newPlot(dataVis, data, layout, {
+      displayModeBar: false
+    });
+    // Logica di selezione
+    dataVis.on('plotly_selected', (eventData) => {
+      nCanali = $("#nCanali").val() || 1000;
+      path1 = $("#path1").val() || path1;
+      path2 = $("#path2").val() || path2;
+      let xRange = eventData.range.x.map(x => toChannel(x, interval, min));
+      let yRange = eventData.range.y.map(x => toChannel(x, interval, min));
+      x1 = xRange[0];
+      x2 = xRange[1];
+      y1 = yRange[0];
+      y2 = yRange[1];
+      console.log(xRange, yRange);
+      updateMatrix(x1, x2, y1, y2, nCanali, path1, path2);
+    });
+    $("#drawButton").html("Reset")
+  });
+})
+
+// Update the current image channel resolution
+$("#updateChannels").on("click", function (e) {
+  nCanali = $("#nCanali").val() || 1000;
+  if (x1 && x2 && y1 && y2)
+    updateMatrix(x1, x2, y1, y2, nCanali, path1, path2);
+  else
+    updateMatrix(x1, x2, y1, y2, nCanali, path1, path2, false);
 });
 
-var dataFile = fs.readFileSync("result.txt", 'ascii');
-console.log("Loaded");
-var dataLines = dataFile.split("\n");
-var metaData = dataLines.shift().split(" ");
-var interval = metaData[0] * 1;
-var min = metaData[1] * 1;
-var xData = dataLines.map(x => x.split(" ")[0] * 1);
-var yData = dataLines.map(x => x.split(" ")[1] * 1);
-var zData = dataLines.map(x => x.split(" ")[2] * 1);
-
-
-var data = [{
-  x: xData,
-  y: yData,
-  z: zData,
-  
-  type: 'histogram2dcontour',
-  line: {
-  	width: 0
-  },
-  /*contours: {
-  	coloring: 'heatmap'
-  },*/
-  //type: 'heatmap',
-  colorscale: [[0, 'rgb(200,200,255)'], [0.75, 'rgb(0, 255, 0)'], [0.99, 'rgb(255,0,0)'], [1, 'rgb(255,0,0)']]
-}];
-var xAxisTemplate = {
-  range: [0, nCanali],
-  showgrid: true,
-  zeroline: true,
-  linecolor: 'black',
-  showticklabels: true,
-  ticks: ''
-};
-
-var yAxisTemplate = {
-  showgrid: true,
-  zeroline: true,
-  linecolor: 'black',
-  showticklabels: true,
-  ticks: ''
-};
-var layout = {
-  xaxis: xAxisTemplate,
-  yaxis: yAxisTemplate,
-  dragmode: 'select'
-};
-var dataVis = document.getElementById('dataVis');
-Plotly.newPlot(dataVis, data, layout, {
-  displayModeBar: false
-});
-
-// Logica di selezione
-dataVis.on('plotly_selected', (eventData) => {
-  let xRange = eventData.range.x.map(x => toChannel(x, interval, min));
-  let yRange = eventData.range.y.map(x => toChannel(x, interval, min));
-  console.log(xRange, yRange);
-  let parameters = [path1, path2, xRange[0], xRange[1], yRange[0], yRange[1], nCanali, false];
+// Update the matrix plot
+function updateMatrix(x1, x2, y1, y2, numeroCanali, primoPath, secondoPath, useParameters = true) {
+  let parameters;
+  if (useParameters)
+    parameters = [numeroCanali, primoPath, secondoPath, x1, x2, y1, y2, false];
+  else
+    parameters = [numeroCanali];
   child(executablePath, parameters, function (err, data) {
     dataFile = fs.readFileSync("result.txt", 'ascii');
     if (err) console.log("ERRORE: " + err)
@@ -102,10 +127,31 @@ dataVis.on('plotly_selected', (eventData) => {
     dataVis.data[0].x = xData;
     dataVis.data[0].y = yData;
     dataVis.data[0].z = zData;
+    let xAxisTemplate = {
+      range: [0, numeroCanali],
+      showgrid: true,
+      zeroline: true,
+      linecolor: 'black',
+      showticklabels: true,
+      ticks: ''
+    };
+    let yAxisTemplate = {
+      showgrid: true,
+      zeroline: true,
+      linecolor: 'black',
+      showticklabels: true,
+      ticks: ''
+    };
+    dataVis.layout = {
+      xaxis: xAxisTemplate,
+      yaxis: yAxisTemplate,
+      dragmode: 'select'
+    };
     Plotly.redraw(dataVis);
   });
-});
+}
 
+// reutrn the real channel
 function toChannel(x, interval, min) {
   return Math.floor(x * interval + min);
 }
