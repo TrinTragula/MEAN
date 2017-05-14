e1327b6127e3e262b7884f883da23b168f9fdf75
TrinTragula <dcat@protonmail.com>
2017-05-14 19:19:07 +0200

    Tab, salvataggio, immagini, pulizia

--- Node/main.js
+++ Node/main.js
@@ -3,20 +3,37 @@ const {
     app,
     BrowserWindow
 } = electron;
+const dialog = electron.dialog;
+const fs = require('fs');
 
+var win = null;
 
 app.on('ready', () => {
-    let win = new BrowserWindow({width: 1200, height:850});
+    win = new BrowserWindow({
+        width: 1200,
+        height: 850,
+        icon: './icon.png'
+    });
     //win.maximize()
     win.loadURL(`file://${__dirname}/index.html`);
     win.webContents.openDevTools();
 })
 
-exports.openWindow = () => {
-    let win = new BrowserWindow({
-        width: 400,
-        height: 200
-    });
-    win.loadURL(`file://${__dirname}/bear.html`);
-}
+app.on('window-all-closed', app.quit);
 
+const saveFile = function (content) {
+    var fileName = dialog.showSaveDialog(win, {
+        title: 'Save database',
+        defaultPath: "*/coincidenze_vere.sqlite",
+        filters: [{
+            name: 'Sqlite Database(s)',
+            extensions: ['sqlite']
+        }]
+    });
+    if (!fileName) {
+        return;
+    }
+    fs.writeFileSync(fileName, content);
+};
+
+exports.saveFile = saveFile;
