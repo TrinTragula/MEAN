7bc1c8c73d10f025ce83c14bf8e4ab8d28f811ac
TrinTragula <dcat@protonmail.com>
2017-11-26 13:28:03 +0100

    Backround più carino, autopicchi stupendi anche su specturm, la vita è bella

---
+++ CSharp/DataCruncher/Analysis/PeaksSpectrum.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCruncher.Analysis
+{
+    public static class PeaksSpectrum
+    {
+        public static void GetPeaks(string fileName, int[] data, int epsilon, int treshold, int multiplier)
+        {
+            // Avoid noise artifacts
+            data[0] = 0;
+            data[1] = 0;
+            // Get points
+            var maxPoint = LocalMaxima(data.ToArray(), epsilon * multiplier, treshold).ToArray();
+            // Write them to a file
+            using (StreamWriter writetext = new StreamWriter(String.Format("{0}.txt", fileName)))
+            {
+                for (var j = 0; j < maxPoint.Length; j++)
+                {
+                    writetext.WriteLine("{0} {1}", (double) maxPoint[j].Item1 / multiplier, maxPoint[j].Item2);
+                }
+            }
+            return;
+        }
+
+        /// <summary>
+        /// A function to get the peaks from a set of data
+        /// </summary>
+        /// <param name="source"> Data </param>
+        /// <param name="windowSize"> Width of the peak window </param>
+        /// <param name="treshold"> Minimum value for the peak not to be considered noise </param>
+        public static IEnumerable<Tuple<int, int>> LocalMaxima(IEnumerable<int> source, int windowSize, int treshold)
+        {
+            // Round up to nearest odd value
+            windowSize = windowSize - windowSize % 2 + 1;
+            int halfWindow = windowSize / 2;
+
+            int index = 0;
+            var before = new Queue<int>(Enumerable.Repeat(int.MinValue, halfWindow));
+            var after = new Queue<int>(source.Take(halfWindow + 1));
+
+            foreach (int d in source.Skip(halfWindow + 1).Concat(Enumerable.Repeat(int.MinValue, halfWindow + 1)))
+            {
+                int curVal = after.Dequeue();
+                if (before.All(x => curVal > x) && after.All(x => curVal >= x) && curVal > treshold)
+                {
+                    yield return Tuple.Create(index, curVal);
+                }
+
+                before.Dequeue();
+                before.Enqueue(curVal);
+                after.Enqueue(d);
+                index++;
+            }
+        }
+    }
+}
