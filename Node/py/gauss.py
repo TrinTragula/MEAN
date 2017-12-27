7540bdefe501507450614139bd92bc2d35037ce2
TrinTragula <dcat@protonmail.com>
2017-12-27 16:42:42 +0100

    Bug fitting per dati con x non intero

--- Node/py/gauss.py
+++ Node/py/gauss.py
@@ -3,19 +3,30 @@ from numpy import exp
 from scipy.optimize import curve_fit
 import sys
 
-data = loadtxt(sys.argv[1])
+fileData = loadtxt(sys.argv[1])
 peaks = loadtxt(sys.argv[2])
 peak = int(sys.argv[3])
 window = int(sys.argv[4])
 
 p = int(peaks[peak][0])
 point = float(peaks[peak][0])
-data = list(map(lambda x: x[1], data))
-
+data = list(map(lambda x: x[1], fileData))
 left = p - window / 2
 right = p + window / 2
 
-data = data[left:right]
+realData = []
+baseline = []
+i = 0
+print(left,right)
+for d in data:
+    v = fileData[i][0]
+    if (v >= left and v <= right ):
+        realData.append(d)
+        baseline.append(fileData[i][0])
+    i += 1
+
+data = realData
+print(baseline)
 
 # Define model function to be used to fit to the data above:
 def gaussian(x, *p):
@@ -24,12 +35,14 @@ def gaussian(x, *p):
 
 # p0 is the initial guess for the fitting coefficients (A, mu and sigma above)
 p0 = [1., p, 1.]
-baseline = range(left, right)
-
-best_vals, covar = curve_fit(gaussian, baseline, data, p0)
-area = best_vals[0]
-mu = best_vals[1]
-sigma = best_vals[2]
-
-with open("data/calibrating_{0}_fitData.txt".format(peak), 'w+') as f:
-    f.write("{0}:{1}:{2}".format(area,mu,sigma))
+try:
+    best_vals, covar = curve_fit(gaussian, baseline, data, p0)
+    area = best_vals[0]
+    mu = best_vals[1]
+    sigma = best_vals[2]
+    print(best_vals)
+    with open("data/calibrating_{0}_fitData.txt".format(peak), 'w+') as f:
+        f.write("{0}:{1}:{2}".format(area,mu,sigma))
+except:
+    with open("data/calibrating_{0}_fitData.txt".format(peak), 'w+') as f:
+        f.write("{0}:{1}:{2}".format("NaN", "NaN", "NaN"))
