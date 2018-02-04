3a6b384a047781873456ad410d97973642a6b11a
TrinTragula <daniele.scarinci@protonmail.com>
2018-02-04 12:27:57 +0100

    Migliori tabelle in interfaccia toi

--- Node/toi.js
+++ Node/toi.js
@@ -9,7 +9,7 @@ const formatter = d3.format('.2f');
 const Toi = require('./classes/toi');
 
 function appendLineToTable(line, element) {
-    var stringToAppend = `<td class="box">${line.Energy}</td><td class="box">${isNaN(line.Intensity) ? "-" : line.Intensity}</td><td class="box">${line.Element}</td><td class="box">${line.Decay}</td>`;
+    var stringToAppend = `<tr><td>${line.Energy}</td><td>${isNaN(line.Intensity) ? "-" : line.Intensity}</td><td>${line.Element}</td><td>${line.Decay}</td></tr>`;
     element.append(stringToAppend);
 }
 
@@ -26,13 +26,13 @@ $(function () {
             to = to ? to * 1 : from;
             Toi.getGammaAtEnergyRange(from, to).then(data => {
                 var table = $("#toiTable");
-                table.append("<tr><th class='boxth'>Energy</th><th class='boxth'>Intensity</th><th class='boxth'>Element</th><th class='boxth'>Decay</th></tr>")
+                table.append("<thead><tr><th class='boxth'>Energy</th><th class='boxth'>Intensity</th><th class='boxth'>Element</th><th class='boxth'>Decay</th></tr></thead>")
+                table.append("<tbody>");
                 for (var x of data) {
                     //console.log(x);
-                    table.append("<tr>");
                     appendLineToTable(x, table);
-                    table.append("</tr>");
                 }
+                table.append("</tbody>");
             });
         }
     });
@@ -50,12 +50,14 @@ $(function () {
             Toi.getPossibleElementsWithError(energies, error).then(data => {
                 var table = $("#toiTable");
                 console.log(data);
-                table.append("<tr><th class='boxth'>Possible elements</th></tr>")
+                table.append("<thead><tr><th class='boxth'>Possible elements</th></tr></thead>");
+                table.append("<tbody>");
                 for (var x of data) {
                     //console.log(x);
-                    table.append("<tr><td style='text-align: center;border: solid 1px black;padding: 10px;'>" + x +
+                    table.append("<tr><td padding: 10px;'>" + x +
                         "<a href='https://www.nndc.bnl.gov/nudat2/getdataset.jsp?nucleus=" + x + "&unc=nds' target='_blank'> (Nudat) </a></td></tr>");
                 }
+                table.append("</tbody>");
             });
         }
     });
