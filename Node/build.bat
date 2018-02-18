1f6d3d18bad92f269f793ad8f772b93f2d05e012
TrinTragula <dcat@protonmail.com>
2018-02-18 21:10:52 +0100

    Fix metadati vari

--- Node/build.bat
+++ Node/build.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
-@RD /S /Q coincidence-win32-x64
-electron-packager . --icon=icon.ico --overwrite
-REM mkdir "coincidence-win32-x64\DataCruncher"
-REM mkdir "coincidence-win32-x64\data"
-REM MOVE "coincidence-win32-x64\resources\app\DataCruncher" "coincidence-win32-x64\DataCruncher\"
-REM MOVE "coincidence-win32-x64\resources\app\data" "coincidence-win32-x64\data\"
+@RD /S /Q "D:/temp/mean-win32-x64"
+
+electron-packager . --icon=icon.ico --overwrite --out D:/temp
+
+@COPY "D:/temp/mean-win32-x64/resources/app/DataCruncher" "D:/temp/mean-win32-x64/DataCruncher"
+@COPY "D:/temp/mean-win32-x64/resources/app/py" "D:/temp/mean-win32-x64/py"
+@MKDIR "D:/temp/mean-win32-x64/data"
+@COPY "C:/Users/Daniele/Desktop/Tesi/coincidenze_vere.sqlite" "D:/temp/mean-win32-x64/coincidenze_vere.sqlite"
